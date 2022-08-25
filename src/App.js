54a413d9eb26992590bcb3f68d700e5446ee3a7c
Brian Oshinbolu <oshinbolubrian@gmail.com>
2022-08-25 20:42:28 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react-dom'
-import './App.css';
+import "./style_component/App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <h1>This is the beginning</h1>
       </header>

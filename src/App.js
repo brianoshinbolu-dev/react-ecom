d7c407b58e6b4fae11fe3242a2aa79ce5e8c7e01
Brian Oshinbolu <oshinbolubrian@gmail.com>
2022-08-25 19:31:02 +0100

    creating the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is the beginning</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
